4b459920c7ff9105e6cb81b3df3bf018c43757a0
Johnni AI <100820048+johnniesue@users.noreply.github.com>
2025-07-07 20:43:27 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,51 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('a1apsvc-dashboard-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js'
-      ]);
+const CACHE_NAME = 'a1apsvc-dashboard-cache-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './schedule.html',
+  './reports.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './images/icon-192.png',
+  './images/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
-});
